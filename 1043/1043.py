f3e9985f311ae0ba21092c759332e147851e5619
Jabba <75074825+suba351@users.noreply.github.com>
2021-02-22 22:02:39 +0300

    Update 1043.py

--- 1043/1043.py
+++ 1043/1043.py
@@ -2,12 +2,18 @@ class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
         dp = [0] * n
-        for i in range(n):
+        max_value = arr[0]
+        for i in range(k):
+            max_value = max(max_value, arr[i])
+            dp[i] = max_value * (i + 1)
+        for i in range(k, n):
+            max_value = 0
+            max_ans = 0
             for j in range(0, k):
-                if i-j <= 0:
-                    dp[i] = max(arr[0:i + 1]) * (i + 1)
-                    break
-                else:
-                    dp[i] = max(dp[i], dp[i - j - 1] + max(arr[i - j: i + 1]) * (j + 1))
+                max_value = max(max_value, arr[i - j])
+                max_ans = max(max_ans, dp[i - j - 1] + max_value * (j + 1))
+            dp[i] = max_ans
+
         print(dp)
         return dp[-1]
+    
